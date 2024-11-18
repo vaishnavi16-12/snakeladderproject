9f4db197ffbff39e00ca9d194b2cc2256325ac22
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-18 18:01:19 +0530

    Snake Ladder Project

---
+++ src/com/bridgelabz/snakeproject/SnakeLadderMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snakeproject;
+
+public class SnakeLadderMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake Ladder project");
+
+        }
+    }
