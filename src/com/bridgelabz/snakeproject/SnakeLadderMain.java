2e954e56480a9d12f89d601ed3641bc096143047
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-19 12:18:58 +0530

    Added: The Player rolls the die to get a number between 1 to 6

--- src/com/bridgelabz/snakeproject/SnakeLadderMain.java
+++ src/com/bridgelabz/snakeproject/SnakeLadderMain.java
@@ -6,34 +6,58 @@ public class SnakeLadderMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder project");
 
+        int playerPosition = 0;
+        int[] snakes = new int[101];
+        int[] ladders = new int[101];
 
-            int playerPosition = 0;
-            Random dice = new Random();
-            int[] snakes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            int[] ladders = {0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 67, 0, 0, 0, 0, 0, 0, 0, 91, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        snakes[16] = 6;
+        snakes[47] = 26;
+        snakes[49] = 11;
+        snakes[56] = 53;
+        snakes[62] = 19;
+        snakes[64] = 60;
+        snakes[87] = 24;
+        snakes[93] = 73;
+        snakes[95] = 75;
+        snakes[98] = 78;
 
-            while (playerPosition < 100) {
-                int roll = dice.nextInt(6) + 1;
-                System.out.println("You rolled a " + roll);
-                playerPosition += roll;
+        ladders[2] = 38;
+        ladders[7] = 29;
+        ladders[8] = 31;
+        ladders[15] = 26;
+        ladders[21] = 42;
+        ladders[28] = 84;
+        ladders[36] = 44;
+        ladders[51] = 67;
+        ladders[71] = 91;
+        ladders[80] = 100;
 
-                if (playerPosition > 100) {
-                    playerPosition = 100;
-                }
+        Random dice = new Random();
 
+        while (playerPosition < 100) {
+            int roll = (int) (Math.random() * 6) + 1;
+            System.out.println("You rolled a " + roll);
+            playerPosition += roll;
+
+            if (playerPosition > 100) {
+                playerPosition = 100;
+            }
+
+            if (playerPosition <= 100) {
                 if (snakes[playerPosition] != 0) {
                     System.out.println("Oops! You landed on a snake! Moving to position " + snakes[playerPosition]);
                     playerPosition = snakes[playerPosition];
-                }
-
-                if (ladders[playerPosition] != 0) {
+                } else if (ladders[playerPosition] != 0) {
                     System.out.println("Great! You landed on a ladder! Climbing to position " + ladders[playerPosition]);
                     playerPosition = ladders[playerPosition];
                 }
-
-                System.out.println("You are now at position: " + playerPosition);
             }
 
-            System.out.println("Congratulations! You've reached the end!");
+            System.out.println("You are now at position: " + playerPosition);
         }
+
+        System.out.println("Congratulations! You've reached the end!");
     }
+}
+
+
