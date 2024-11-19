90b528a64192d7724e7367b28ac997d91724fb55
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-19 12:04:55 +0530

    Added:SnakeLaddergame played with single player at start position 0

--- src/com/bridgelabz/snakeproject/SnakeLadderMain.java
+++ src/com/bridgelabz/snakeproject/SnakeLadderMain.java
@@ -1,8 +1,39 @@
 package com.bridgelabz.snakeproject;
 
+import java.util.Random;
+
 public class SnakeLadderMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake Ladder project");
 
+
+            int playerPosition = 0;
+            Random dice = new Random();
+            int[] snakes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] ladders = {0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 67, 0, 0, 0, 0, 0, 0, 0, 91, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            while (playerPosition < 100) {
+                int roll = dice.nextInt(6) + 1;
+                System.out.println("You rolled a " + roll);
+                playerPosition += roll;
+
+                if (playerPosition > 100) {
+                    playerPosition = 100;
+                }
+
+                if (snakes[playerPosition] != 0) {
+                    System.out.println("Oops! You landed on a snake! Moving to position " + snakes[playerPosition]);
+                    playerPosition = snakes[playerPosition];
+                }
+
+                if (ladders[playerPosition] != 0) {
+                    System.out.println("Great! You landed on a ladder! Climbing to position " + ladders[playerPosition]);
+                    playerPosition = ladders[playerPosition];
+                }
+
+                System.out.println("You are now at position: " + playerPosition);
+            }
+
+            System.out.println("Congratulations! You've reached the end!");
         }
     }
